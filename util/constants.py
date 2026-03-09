2e6321844cf0f3157b6978949cd8bbf1df3a6767
ekp529 <ekpaupulaire@gmail.com>
2026-03-09 10:37:31 -0400

    finished airflow config

--- util/constants.py
+++ util/constants.py
@@ -7,6 +7,7 @@ DOCKER_KAFKA_BOOTSTRAP_SERVER = ["kafka:9092"]
 LOCAL_KAFKA_BOOTSTRAP_SERVER = ["localhost:9094"]
 KAFKA_BATCH_SIZE = 100        # Messages per batch
 KAFKA_BATCH_INTERVAL = 5     # Seconds before writing batch even if not full
+MAX_KAFKA_CONSUMER_IDLE_TIME = 10  # seconds before exiting kafka consumer task
 
 DOCKER_MINIO_ENDPOINT = "http://minio:9000"
 LOCAL_MINIO_ENDPOINT = "http://localhost:9000"
