7ac0d4c689fd5167070d8ba7997a9761f25195b6
dmueller34 <daniel@zerocool.us>
2026-03-18 15:07:30 -0400

    minor change

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import subprocess
-
 import streamlit as st
 import pandas as pd
 import numpy as np
--- pages/ingestion.py
+++ pages/ingestion.py
@@ -1,5 +1,3 @@
-import subprocess
-
 import numpy as np
 import streamlit as st
 import pandas as pd
